7e116382aedd34925cecffd15fdd232f0a933192
LiangErika <126982877+LiangErika@users.noreply.github.com>
2023-08-31 08:04:14 -0700

    clienteDatos.js

--- clienteDatos.js
+++ clienteDatos.js
@@ -24,6 +24,7 @@ servicio.getDatos(1,3000,function(){
 })*/
 
 //Promises
+/*
 servicio.getDatos(1,1000)
 .then(()=> servicio.getDatos(2,500))
 .then(()=>servicio.getDatos(3,1500))
@@ -31,3 +32,20 @@ servicio.getDatos(1,1000)
 .then(()=>servicio.getDatos(5,3500))
 .then(()=>{console.log("Programa terminada");})
 .catch(error => {console.log("Error: ",error);});
+*/
+
+//Await
+async function  procesa(){
+    try{
+        await servicio.getDatos(1,1000);
+        await servicio.getDatos(2,500);
+        await servicio.getDatos(3,4000);
+        await servicio.getDatos(4,700);
+        await servicio.getDatos(5,3500);
+        console.log("Programa terminado");
+    }catch(error){
+        console.log(error);
+    }
+}
+procesa();
+
